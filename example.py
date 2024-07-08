6f1cedefec46bdc0979175e1e6dd753d1b1657d2
lwolf1 <laura.wolf@cs.uni-goettingen.de>
2024-07-08 10:04:03 +0200

    updated example with herded variant

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from figures.plot_util import plot_single_association
 import numpy as np
 from scipy.stats import multivariate_normal
 
-from t2ta_sampling import t2ta_stochastic_optimization
+from StochasticOptimization import T2TA_SO
 from greedy_multidim import greedy_t2ta
 
 
@@ -37,24 +37,41 @@ np.random.shuffle(shuffle_idx)
 tracks = tracks[shuffle_idx]
 gt_asso = gt_asso[shuffle_idx]
 
+
+# greedy with merging
 greedy_asso = greedy_t2ta(tracks)
 
-samples, weights = t2ta_stochastic_optimization(tracks, num_sweeps, num_sensors, detection_prob)
+SO_random = T2TA_SO(detection_prob, 'random')
+SO_herded = T2TA_SO(detection_prob, 'herded')
+SO_herded_gated = T2TA_SO(detection_prob, 'herded_gated')
+SO_ML = T2TA_SO(detection_prob, 'ML')
 
-best_idx_rnd = weights.argmax()
-best_sample_rnd = samples[best_idx_rnd]
+so_asso_random, _ = SO_random.associate(tracks,num_sweeps,num_sensors)
+so_asso_herded, _ = SO_herded.associate(tracks,num_sweeps,num_sensors)
+so_asso_herded_gated, _ = SO_herded_gated.associate(tracks,num_sweeps,num_sensors)
+so_asso_ML, _ = SO_ML.associate(tracks,num_sweeps,num_sensors)
+
+
 
 fig, ax = plt.subplots()
 plot_single_association(gt_asso, gt, tracks, ax)
 ax.set_title('Ground truth')
 
 fig2, ax2 = plt.subplots()
-plot_single_association(best_sample_rnd, gt, tracks, ax2)
+plot_single_association(so_asso_random, gt, tracks, ax2)
 ax2.set_title('Stochastic optimization')
 
 fig3, ax3 = plt.subplots()
 plot_single_association(greedy_asso, gt, tracks, ax3)
 ax3.set_title('Greedy with merging')
+
+fig4, ax4 = plt.subplots()
+plot_single_association(so_asso_random, gt, tracks, ax4)
+ax4.set_title('Herded Stochastic optimization')
+
+fig5, ax5 = plt.subplots()
+plot_single_association(so_asso_random, gt, tracks, ax5)
+ax5.set_title('Gated Herded Stochastic optimization')
 
 
 plt.show()
